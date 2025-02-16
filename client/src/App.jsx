1bb36578110e8f08deeccbcc963a0bf4c6dde4e9
moham <mohammedsuhaibv@gmail.com>
2025-02-16 17:28:47 +0530

    cursor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import Home from './pages/Home/Home';
 import Loader from './components/Loading/Hamsterloader';
+import CustomCursor from './components/Cursor/Cursor'
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -12,10 +13,14 @@ const App = () => {
     }, 4000);
 
     return () => clearTimeout(timer);
-  }, []);
+  },
+  
+  []);
 
   return (
+    
     <div className="landing-page">
+      <CustomCursor />
       {loading ? (
         <div className="loader-container">
           <Loader />
